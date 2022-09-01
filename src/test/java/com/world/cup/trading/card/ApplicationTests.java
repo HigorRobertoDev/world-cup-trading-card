ad03348559a81c4bb09a068c7d9a7ef76e99966d
HigorRobertoDev <h.saposilva@gmail.com>
2022-09-01 10:21:50 -0300

    first commit

---
+++ src/test/java/com/world/cup/trading/card/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.world.cup.trading.card;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
